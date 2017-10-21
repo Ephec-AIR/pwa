a0641b89ee2bd499131230fa1f9fcf06ba6839c1
Mathieu-R <mathieu0709@gmail.com>
2017-10-21 10:59:00 +0200

    nav

--- src/components/air-nav.vue
+++ src/components/air-nav.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="air-nav">
     <div class="air-nav--inner">
-      <nav class="air-nav--inner__nav-container">
+      <nav class="air-nav--inner__nav-container" role="navigation">
         <ul class="air-nav--inner__nav-content">
           <li>
             <router-link to="/home" class="air-nav__inner__nav-link" aria-label="home">Home</router-link>
@@ -26,5 +26,30 @@ export default {
 
 
 <style lang="scss">
+  .air-nav--inner__nav-container {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    background: #FFF;
+    width: 100%;
 
+    .air-nav--inner__nav-content {
+      margin: 0;
+      padding: 0;
+      max-height: 80px;
+      display: flex;
+      list-style: none;
+
+      li {
+        margin: 5px 10px;
+        padding: 5px;
+      }
+
+      .air-nav__inner__nav-link {
+        font-size: 16px;
+        color: #37474F;
+        text-decoration: none;
+      }
+    }
+  }
 </style>
