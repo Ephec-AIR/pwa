d1eccc63768a5ccd971bbbd1da45dc163ebc301f
Nathan Dolinski <nathan@dado.be>
2017-11-14 09:16:26 +0100

    Modified profile form

--- src/components/air-param.vue
+++ src/components/air-param.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="air-param">
     <div class="air-param--block">
-        <form class="air-param--form">
-           <input type="text" name="nameText" placeholder="Nom">
-           <input type="text" placeholder="Prénom">
-           <input type="text" placeholder="Mail">
-           
-            
+        <form action="#" method="post" name="profile" class="air-param-form--profile">
+          <fieldset>
+            <legend>Mon profil</legend>
+            <label for="lastnameProfile">
+              Nom : <input type="text" name="lastnameProfile" placeholder="Nom">
+            </label>
+            <label for="firstnameProfile">
+                Prénom : <input type="text" name = "firstnameProfile" placeholder="Prénom">
+            </label>
+            <label for="mailProfile">
+                Mail: <input type="text" name="mailProfile" placeholder="Mail"> 
+            </label>   
+           </fieldset>      
         </form>
     </div>
   </div>
@@ -20,17 +27,19 @@
     height: 100%;
     display: flex;
     flex-direction: row;
+    background-color: #FFD400;
         .air-param--block{
             position: absolute;
             border-radius: 5px;
             left:35%;
             top:30%;
             height:50%;
-            width: 50%;
+            width: 30%;
             margin: auto;
             border: 1px solid black;
             box-shadow: 10px 5px 5px black;
-            .air-param--form{
+            background-color: #FFF;
+            .air-param-form--profile{
             position: relative;
             display: flex;
             flex-direction: column;
@@ -41,7 +50,7 @@
                 input{
                     text-align: center;
                     border-radius: 5px;
-                    width: 50%;
+                    width: 100%;
                     height: 30px;
                     margin-left: auto;
                     margin-right:auto;
