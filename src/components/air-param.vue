761ce501588ca38eaa84c9117ce326ed32f5d5ce
Natherc <nathan@dado.be>
2017-11-14 22:44:27 +0100

    Modified forms parameters

--- src/components/air-param.vue
+++ src/components/air-param.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="air-param">
     <div class="air-param--block">
-        <form action="#" method="post" name="profile" class="air-param-form--profile">
-          <fieldset>
-            <legend>Mon profil</legend>
+      <ul class="param-list">
+        <li><button class="myParam" @click="toggleForm">Mes paramètres</button></li>
+        <li><button class ="OCR" @click="toggleForm">OCR</button></li>
+      </ul>
+      <transition name='fade'>
+        <form action="#" method="post" name="profile" class="air-param-form--profile" v-if='display === 1'>          
             <label for="lastnameProfile">
               Nom : <input type="text" name="lastnameProfile" placeholder="Nom">
             </label>
@@ -12,14 +15,45 @@
             </label>
             <label for="mailProfile">
                 Mail: <input type="text" name="mailProfile" placeholder="Mail"> 
-            </label>   
-           </fieldset>      
+            </label>             
         </form>
+      </transition>
+      <transition name='fade'>
+         <form action="#" method="post" name="profile" class="air-param-form--OCR" v-if='display === 2'>          
+            <label for="lastnameProfile">
+              N° série : <input type="text" name="serialOCR" placeholder="numéro de série">
+            </label>
+                       
+        </form>
+      </transition>  
+        
     </div>
   </div>
 </template>
 
 <script>
+  export default{
+    data (){
+
+      return {
+
+        display: 1  
+
+      }
+
+    },
+    methods: {
+        toggleForm(e){
+         
+            if(e.target.classList.contains('myParam')){
+                this.display = 1
+            }else{
+              this.display = 2
+            }
+        }
+      }  
+    }
+   
 </script>
 
 <style lang="scss">
@@ -34,12 +68,38 @@
             left:35%;
             top:30%;
             height:50%;
-            width: 30%;
+            width: 50%;
             margin: auto;
             border: 1px solid black;
             box-shadow: 10px 5px 5px black;
             background-color: #FFF;
-            .air-param-form--profile{
+            .param-list{
+              display : flex;
+              list-style: none;
+               & li {
+                 margin:auto;
+
+                 & button{  
+
+                 border: none;
+                 background-color: #01a875;
+                 margin : auto;
+                 border-radius : 5px;
+                 transition: transform 0.2s ease-in,background-color 0.2s ease-in;
+                 transform : scale(1.5);
+
+                 &:hover{
+                   transform : scale(2);
+                   z-index: 1;
+                   background-color: red;
+                 }
+
+                 }
+               }    
+               
+            }
+ 
+            form{
             position: relative;
             display: flex;
             flex-direction: column;
@@ -47,6 +107,7 @@
             height: 100%;
             width: 100%; 
             margin: auto;
+            padding : 10px;
                 input{
                     text-align: center;
                     border-radius: 5px;
@@ -60,6 +121,17 @@
         
 
                     
+        }
+
+        .fade-enter-active, .fade-leave-active {
+          transition: opacity 0.5s ease-in,transform 0.5s ease-in;
+        }
+
+        .fade-enter, .fade-leave-active{
+          opacity: 0;
+          transform: translateY(50px);
+          z-index: 1;
+          
         }
 
         }
