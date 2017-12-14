a2f4ee6a2ab69d249cd7679b5d21e54e9d1364d5
Mathieu-R <mathieu0709@gmail.com>
2017-12-14 16:54:28 +0100

    fix test

--- test/integrations.js
+++ test/integrations.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   try {
-    const browser = await puppeteer.launch({slowMo: 250});
+    const browser = await puppeteer.launch({slowMo: 100});
     const page = await browser.newPage();
 
     await page.goto('https://air.ephec-ti.org/');
@@ -11,17 +11,19 @@ const puppeteer = require('puppeteer');
     await page.type('input[name=password]', 'test123');
     await page.click('.air-login-form--button-login');
 
-    await page.screenshot({path: 'test/connectedToHomePage.png'});
+    await page.waitForSelector('.air-graph-controls__container');
+    //await page.screenshot({path: 'test/connectedToHomePage.png'});
 
     await page.click('a[href="/parameters"]');
-    await page.screenshot({path: 'test/parameters.png'});
+    //await page.screenshot({path: 'test/parameters.png'});
 
     await page.click('a[href="https://air.ephec-ti.org/forum/"]');
-    await page.screenshot({path: 'test/forum.png'});
+    //await page.screenshot({path: 'test/forum.png'});
 
     await browser.close();
+    console.log('SUCCESS !');
   } catch (err) {
-    console.error(err)
+    console.error('LOGIN FAILED !');
+    console.error(err);
   }
 })();
-
